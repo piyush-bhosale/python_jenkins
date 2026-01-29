6a453d24637177374b8ac7947e2d46f823a6aa94
piyush.bhosale <142383440+piyush-bhosale@users.noreply.github.com>
2026-01-29 11:13:11 +0530

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,9 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    return a - b
+
+if __name__ == "__main__":
+    print("add(5, 3) =", add(5, 3))
+    print("subtract(5, 3) =", subtract(5, 3))
