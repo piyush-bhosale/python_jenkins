8b301587ec9ff3feb05564ede588cff4e5806b14
piyush.bhosale <142383440+piyush-bhosale@users.noreply.github.com>
2026-01-29 11:11:48 +0530

    Update and rename app.py to requirement.txt

--- app.py
+++
@@ -1,9 +0,0 @@
-def add_numbers(a, b):
-    return a + b
-
-def subtract_numbers(a, b):
-    return a - b
-
-if __name__ == "__main__":
-    print("Adding 5 + 3 =", add_numbers(5, 3))
-    print("Subtracting 5 - 3 =", subtract_numbers(5, 3))
